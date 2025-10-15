8091bbd0f25f15100c14055b237485b21cdbf19f
michalmatysik6 <michal.matysik6@gmail.com>
2025-10-15 02:07:06 +0200

    Update Token.java

--- src/main/java/edu/io/Token.java
+++ src/main/java/edu/io/Token.java
@@ -1,1 +1,13 @@
+package edu.io;
 
+public class Token {
+    private String label;
+    
+    public Token(String label) {
+        this.label = label;
+    }
+    
+    public String getLabel() {
+        return label;
+    }
+}
