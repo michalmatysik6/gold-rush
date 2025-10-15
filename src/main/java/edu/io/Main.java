822d52a77483808e9d8fb6350b6ab71cb8e54f44
michalmatysik6 <michal.matysik6@gmail.com>
2025-10-15 14:58:41 +0200

    Update Main.java

--- src/main/java/edu/io/Main.java
+++ src/main/java/edu/io/Main.java
@@ -3,13 +3,5 @@ package edu.io;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Gold Rush");
-        
-        Board board = new Board(8);
-        
-        Token gold = new Token("G");
-        Token player = new Token("P");
-        
-        board.placeToken(2, 3, gold);
-        board.placeToken(5, 5, player);
     }
 }
