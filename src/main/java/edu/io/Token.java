02a84dc8ab93b6ade7adf2b35969281145ed3f39
michalmatysik6 <michal.matysik6@gmail.com>
2025-10-17 21:38:42 +0200

    Create Token class with label property and methods

--- src/main/java/edu/io/Token.java
+++ src/main/java/edu/io/Token.java
@@ -1,1 +1,17 @@
+package edu.io;
 
+public class Token {
+    private String label;
+    
+    public Token(String label) {
+        this.label = label;
+    }
+    
+    public String getLabel() {
+        return label;
+    }
+    
+    public void setLabel(String label) {
+        this.label = label;
+    }
+}
