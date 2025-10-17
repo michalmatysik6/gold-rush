ac6c26e451cc356f758bb64051c7d46b8d71a37c
michalmatysik6 <michal.matysik6@gmail.com>
2025-10-17 21:40:14 +0200

    Implement Board class with token management

--- src/main/java/edu/io/Board.java
+++ src/main/java/edu/io/Board.java
@@ -1,1 +1,42 @@
+package edu.io;
 
+public class Board {
+    private int size;
+    private Token[][] grid;
+    
+    public Board(int size) {
+        this.size = size;
+        this.grid = new Token[size][size];
+        clean();
+    }
+    
+    public void clean() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                grid[i][j] = new Token("\u30FB");
+            }
+        }
+    }
+    
+    public void placeToken(int col, int row, Token token) {
+        if (col >= 0 && col < size && row >= 0 && row < size) {
+            grid[row][col] = token;
+        }
+    }
+    
+    public Token square(int col, int row) {
+        if (col >= 0 && col < size && row >= 0 && row < size) {
+            return grid[row][col];
+        }
+        return null;
+    }
+    
+    public void display() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(grid[i][j].getLabel() + " ");
+            }
+            System.out.println();
+        }
+    }
+}
