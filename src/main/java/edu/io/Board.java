68e5b9a8a6796abea37a120e004edf57232d0b07
michalmatysik6 <michal.matysik6@gmail.com>
2025-10-17 22:03:48 +0200

    Refactor Board class and initialize grid in constructor
    Refactor Board class to use final fields and initialize the board during construction.

--- src/main/java/edu/io/Board.java
+++ src/main/java/edu/io/Board.java
@@ -1,15 +1,23 @@
 package edu.io;
 
 public class Board {
-    private int size;
-    private Token[][] grid;
-    
+    private final int size;
+    private final Token[][] grid;
+
     public Board(int size) {
         this.size = size;
         this.grid = new Token[size][size];
-        clean();
+        initializeBoard();
     }
     
+    private void initializeBoard() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                grid[i][j] = new Token("\u30FB");
+            }
+        }
+    }
+
     public void clean() {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
@@ -17,7 +25,7 @@ public class Board {
             }
         }
     }
-    
+
     public void placeToken(int col, int row, Token token) {
         if (col >= 0 && col < size && row >= 0 && row < size) {
             grid[row][col] = token;
