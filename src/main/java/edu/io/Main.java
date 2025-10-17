e615eb73aea642671417c41a894ffeed2d88ce65
michalmatysik6 <michal.matysik6@gmail.com>
2025-10-17 21:42:21 +0200

    Implement board and token placement in main
    Added board and token placement functionality to main.

--- src/main/java/edu/io/Main.java
+++ src/main/java/edu/io/Main.java
@@ -3,5 +3,15 @@ package edu.io;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Gold Rush");
+        
+        Board board = new Board(8);
+        Token gold = new Token("\uD83D\uDCB0");
+        Token player = new Token("\uC6C3");
+        
+        board.placeToken(2, 2, gold);
+        board.placeToken(1, 1, gold);
+        board.placeToken(0, 0, player);
+        
+        board.display();
     }
 }
