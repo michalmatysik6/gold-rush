49d71aa49b3bc0fcf941f936720d3e613ea47b73
michalmatysik6 <michal.matysik6@gmail.com>
2025-11-03 23:49:55 +0100

    i02w02

--- src/main/java/edu/io/Label.java
+++ src/main/java/edu/io/Label.java
@@ -4,8 +4,8 @@ public class Label {
     public static final Label EMPTY_TOKEN_LABEL = new Label("・");
     public static final Label GOLD_TOKEN_LABEL = new Label("💰");
     public static final Label PLAYER_TOKEN_LABEL = new Label("웃");
-    public static final Label PICKAXE_TOKEN_LABEL = new Label("T"); // Kilof
-    public static final Label ANVIL_TOKEN_LABEL = new Label("K"); // Kowadło
+    public static final Label PICKAXE_TOKEN_LABEL = new Label("⛏️"); // U+2692
+    public static final Label ANVIL_TOKEN_LABEL = new Label("π");
 
     private final String value;
 
@@ -27,4 +27,5 @@ public class Label {
     public String toString() {
         return value;
     }
+
 }
